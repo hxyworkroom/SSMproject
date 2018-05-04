d9002a842c489287ab391a38962b46d047d419c5
toridas <hzzwmy@sina.com>
2018-05-04 14:28:45 +0800

    js 文件

---
+++ store-manage/src/main/webapp/js/test.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    alert("hello world");	
+  });
+});
+$(document).ready(function(){
+  $(".jstest").click(function(){
+    alert("成功！！！");	
+  });
+});
